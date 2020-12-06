78ab15decba938a403c46827a8f8c68593d3c062
grahamcrowell <graham.crowell@gmail.com>
2020-12-05 20:14:10 -0500

    wip

--- src/bot.rs
+++ src/bot.rs
@@ -1,26 +1,45 @@
 #![allow(dead_code)]
 
-use crate::dag::{to_dag, DagDTO};
+use std::collections::HashMap;
+use std::iter::Map;
+
+use petgraph::prelude::*;
+
+use crate::dag::{execution_order, to_dag, DagDTO};
 use crate::data::{Asset, MockDataClient};
 use crate::dto::{CalculationDTO, StrategyDTO};
 use crate::time_series::{TimeSeries1D, TimeStamp};
-use std::collections::HashMap;
 
 pub struct Bot {
     strategy: StrategyDTO,
     dag: DagDTO,
+    // TODO add calc_lkup
+    // calc_lkup: HashMap<String, CalculationDTO>,
+}
+
+pub struct ExecutableBot {
     // TODO replace type with trait DataClient
     data_client: MockDataClient,
+    calculation_status_lkup: HashMap<String, CalculationStatus>,
+}
+
+pub enum CalculationStatus {
+    NotStarted,
+    Complete,
 }
 
 impl Bot {
     pub fn new(strategy: StrategyDTO) -> Box<Self> {
         let dag = to_dag(&strategy).expect("unable to build bot");
-        let data_client = MockDataClient::new();
+        // let calc_lkup: HashMap<String, CalculationDTO> = strategy
+        //     .calculations()
+        //     .iter()
+        //     .map(|calc| (calc.name().to_string(), calc.clone()))
+        //     .collect();
         Box::from(Bot {
             strategy,
             dag,
-            data_client,
+            // calc_lkup,
         })
     }
     fn dag(&self) -> &DagDTO {
@@ -29,6 +48,14 @@ impl Bot {
     fn strategy(&self) -> &StrategyDTO {
         &self.strategy
     }
+    pub fn calc(&self, name: &str) -> Result<&CalculationDTO, &str> {
+        let _calc = self
+            .strategy
+            .calculations()
+            .iter()
+            .find(|calc| calc.name() == name);
+        _calc.ok_or("not found")
+    }
     pub fn queries(&self) -> Vec<&CalculationDTO> {
         self.strategy
             .calculations()
@@ -36,20 +63,22 @@ impl Bot {
             .filter(|c| (c.operation()) == "query")
             .collect()
     }
-
-    fn execute(&self, asset: &Asset, timestamp: &TimeStamp) -> () {
-        let _dag_node_output_lookup: HashMap<String, TimeSeries1D> = self
-            .queries()
-            .iter()
-            .map(|c| {
-                (
-                    c.name().to_string(),
-                    self.data_client.query(asset, timestamp),
-                )
-            })
-            .collect();
-        let _score_calc = self.strategy.score().calculation();
-    }
+    //
+    // pub fn execute(&self, asset: &Asset, timestamp: &TimeStamp) -> () {
+    //     let _dag_node_output_lookup: HashMap<String, TimeSeries1D> = HashMap::new();
+    //
+    //     let _dag_node_output_lookup: HashMap<String, TimeSeries1D> = self
+    //         .queries()
+    //         .iter()
+    //         .map(|c| {
+    //             (
+    //                 c.name().to_string(),
+    //                 self.data_client.query(asset, timestamp),
+    //             )
+    //         })
+    //         .collect();
+    //     let _score_calc = self.strategy.score().calculation();
+    // }
 }
 
 #[cfg(test)]
@@ -57,7 +86,11 @@ mod tests {
     use std::collections::HashMap;
     use std::path::Path;
 
-    use crate::bot::Bot;
+    use petgraph::algo::toposort;
+    use petgraph::graph::NodeIndex;
+
+    use crate::bot::{Bot, CalculationStatus};
+    use crate::dag::execution_order;
     use crate::data::TODAY;
     use crate::dto::from_path;
     use crate::time_series::TimeSeries1D;
@@ -79,19 +112,26 @@ mod tests {
     }
 
     #[test]
-    fn bot() {
-        let bot = bot_fixture();
-        let symbol = "B";
-        let asset = bot
-            .data_client
-            .assets()
-            .get(symbol)
-            .expect(&*format!("Query Failed. Asset not found {:?}", symbol));
-
-        let _dag_node_output_lookup: HashMap<String, TimeSeries1D> = bot
-            .queries()
-            .iter()
-            .map(|c| (c.name().to_string(), bot.data_client.query(asset, &TODAY)))
+    fn execute() {
+        let bot: Box<Bot> = bot_fixture();
+        let execution_order: Vec<&String> = execution_order(bot.dag());
+        let node_data: HashMap<String, TimeSeries1D> = HashMap::new();
+        let node_statuses: HashMap<&String, CalculationStatus> = bot
+            .dag
+            .node_indices()
+            .into_iter()
+            .map(|node_idx: NodeIndex| {
+                (
+                    bot.dag.node_weight(node_idx).unwrap(),
+                    CalculationStatus::NotStarted,
+                )
+            })
             .collect();
+
+        // execute leaves (queries)
+        for node_idx in toposort(&bot.dag, None).expect("unable to toposort").iter() {
+            println!("{:?}", bot.dag.node_weight(*node_idx));
+        }
+        ()
     }
 }
--- src/dag.rs
+++ src/dag.rs
@@ -1,17 +1,26 @@
 #![allow(dead_code)]
 
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::env::current_dir;
 use std::fs::File;
 use std::io::Write;
 
-use petgraph::algo::{connected_components, is_cyclic_directed};
+use petgraph::algo::{connected_components, is_cyclic_directed, toposort};
 use petgraph::dot::{Config, Dot};
-use petgraph::graph::DiGraph;
+use petgraph::graph::{DiGraph, NodeIndex};
 
 use crate::dto::StrategyDTO;
 
 pub type DagDTO = DiGraph<String, String>;
+
+pub fn execution_order(g: &DagDTO) -> Vec<&String> {
+    toposort(g, None)
+        .expect("unable to toposort")
+        .iter()
+        .map(|node_idx: &NodeIndex| g.node_weight(*node_idx).unwrap())
+        .collect()
+}
 
 pub fn to_dot_text(g: &DagDTO) -> String {
     format!("{:?}", Dot::with_config(g, &[Config::EdgeNoLabel]))
@@ -61,33 +70,51 @@ pub fn to_dag(strategy: &StrategyDTO) -> Result<DagDTO, &str> {
 
 #[cfg(test)]
 mod tests {
+    use std::borrow::Borrow;
     use std::collections::HashMap;
     use std::fs::read_to_string;
+    use std::iter::Map;
     use std::path::Path;
 
     use petgraph::algo::{is_cyclic_directed, toposort};
     use petgraph::prelude::*;
     use petgraph::visit::{Bfs, DfsPostOrder};
 
-    use crate::dag::{to_dag, to_dot_file, DagDTO};
+    use crate::dag::{execution_order, to_dag, to_dot_file, DagDTO};
     use crate::dto::{from_path, StrategyDTO};
 
-    type Dag = DiGraph<String, String>;
+    fn strategy_fixture() -> StrategyDTO {
+        from_path(Path::new("strategy.yaml")).expect("unable to load strategy")
+    }
+
+    fn dag_fixture() -> DagDTO {
+        to_dag(strategy_fixture().borrow()).expect("unable to convert to bot")
+    }
+
+    #[test]
+    fn strategy_to_dag() {
+        let strategy = from_path(Path::new("strategy.yaml")).expect("unable to load strategy");
+        let dag = to_dag(&strategy).expect("unable to convert to bot");
+        to_dot_file(&dag);
+        assert_eq!(dag.node_count(), 5);
+        assert_eq!(dag.edge_count(), 6);
+        let nodes = dag
+            .node_indices()
+            .map(|i| dag.node_weight(i).expect("node not found"))
+            .find(|d| d.as_str().eq("sma200"))
+            .expect("sma200 not found");
+        assert_eq!(nodes, "sma200")
+    }
 
     #[test]
     fn traverse_dag_order() {
-        let strategy: StrategyDTO =
-            from_path(Path::new("strategy.yaml")).expect("unable to load strategy");
-        let dag: DagDTO = to_dag(&strategy).expect("unable to convert to bot");
-        // topological sort is dag order !
-        let sorted_node_ids: Vec<NodeIndex> = toposort(&dag, None).expect("unable to toposort");
-        let mut node_order: HashMap<String, usize> = HashMap::new();
-        let mut position = 0usize;
-        for sorted_node_id in sorted_node_ids {
-            println!("{:?}", dag.node_weight(sorted_node_id));
-            node_order.insert(dag.node_weight(sorted_node_id).unwrap().clone(), position);
-            position += 1;
-        }
+        let dag: DagDTO = dag_fixture();
+        let exe_order = execution_order(&dag);
+
+        let node_execution_order_lkup: HashMap<&String, usize> = (0..exe_order.len())
+            .into_iter()
+            .map(|position| (exe_order.get(position).unwrap().clone(), position))
+            .collect();
         let order_constraints = &[
             &["close", "sma50", "sma_diff", "sma_gap"],
             &["close", "sma200", "sma_diff", "sma_gap"],
@@ -96,37 +123,43 @@ mod tests {
             let expected_order = order_constraints[outer_idx];
             for inner_idx in 0..(expected_order.len() - 1) {
                 let a = expected_order[inner_idx];
-                let a_position = node_order.get(a).unwrap();
+                let a_position = node_execution_order_lkup.get(&a.to_string()).unwrap();
                 let b = expected_order[inner_idx + 1];
-                let b_position = node_order.get(b).unwrap();
-                println!("{:?}", (a, b));
-                println!("{:?}", (a_position, b_position));
+                let b_position = node_execution_order_lkup.get(&b.to_string()).unwrap();
+                // println!("{:?}", (a, b));
+                // println!("{:?}", (a_position, b_position));
                 assert!(a_position < b_position);
             }
         }
     }
 
     #[test]
-    fn get_queries() {
-        let strategy = from_path(Path::new("strategy.yaml")).expect("unable to load strategy");
-        let dag = to_dag(&strategy).expect("unable to convert to bot");
+    fn dag_to_dot_file() {
+        let mut dag: DagDTO = DiGraph::new();
+        let node_index_a: NodeIndex = dag.add_node(String::from("A"));
+        let node_index_b = dag.add_node(String::from("B"));
+        let node_index_c = dag.add_node(String::from("C"));
+        let node_index_d = dag.add_node(String::from("D"));
+        dag.add_edge(node_index_a, node_index_b, String::from(""));
+        dag.add_edge(node_index_a, node_index_c, String::from(""));
+        dag.add_edge(node_index_b, node_index_d, String::from(""));
+        dag.add_edge(node_index_c, node_index_d, String::from(""));
         to_dot_file(&dag);
-        println!("is a dag? {}", !is_cyclic_directed(&dag));
-
-        let _nodes: Vec<_> = toposort(&dag, None)
-            .unwrap()
-            .into_iter()
-            .map(|node_id| dag.node_weight(node_id).unwrap().as_str())
-            .collect();
-
-        let sorted_node_ids = toposort(&dag, None).expect("unable to toposort");
-        let leaf_node_idx = sorted_node_ids.get(0).expect("unable to get leaf");
-        let leaf_node = dag
-            .node_weight(*leaf_node_idx)
-            .expect("unable to find node");
-
-        assert_eq!(leaf_node, "close")
+        let expected_output =
+            read_to_string("expected_output.dot").expect("expected_output.dot not found.");
+        let output = read_to_string("output.dot").expect("output.dot not found.");
+        assert_eq!(output, expected_output);
     }
+}
+
+#[cfg(test)]
+mod learn_library {
+    use petgraph::prelude::*;
+
+    use crate::dag::{to_dag, to_dot_file, DagDTO};
+    use crate::dto::from_path;
+    use petgraph::algo::toposort;
+    use std::path::Path;
 
     #[test]
     fn topo() {
@@ -137,7 +170,6 @@ mod tests {
         let a = dag.add_node(String::from("A"));
         dag.add_edge(a, b, String::new());
         dag.add_edge(c, b, String::new());
-
         let sorted_node_ids = toposort(&dag, None).expect("unable to toposort");
 
         let leaf_node_idx = sorted_node_ids.get(0).expect("unable to get leaf");
@@ -194,37 +226,5 @@ mod tests {
             }
         }
         assert_eq!(node, strategy.score().calculation());
-    }
-
-    #[test]
-    fn dag_to_dot_file() {
-        let mut dag: Dag = DiGraph::new();
-        let node_index_a: NodeIndex = dag.add_node(String::from("A"));
-        let node_index_b = dag.add_node(String::from("B"));
-        let node_index_c = dag.add_node(String::from("C"));
-        let node_index_d = dag.add_node(String::from("D"));
-        dag.add_edge(node_index_a, node_index_b, String::from(""));
-        dag.add_edge(node_index_a, node_index_c, String::from(""));
-        dag.add_edge(node_index_b, node_index_d, String::from(""));
-        dag.add_edge(node_index_c, node_index_d, String::from(""));
-        to_dot_file(&dag);
-        let expected_output =
-            read_to_string("expected_output.dot").expect("expected_output.dot not found.");
-        let output = read_to_string("output.dot").expect("output.dot not found.");
-        assert_eq!(output, expected_output);
-    }
-
-    #[test]
-    fn strategy_to_dag() {
-        let strategy = from_path(Path::new("strategy.yaml")).expect("unable to load strategy");
-        let dag = to_dag(&strategy).expect("unable to convert to bot");
-        assert_eq!(dag.node_count(), 5);
-        assert_eq!(dag.edge_count(), 6);
-        let nodes = dag
-            .node_indices()
-            .map(|i| dag.node_weight(i).expect("node not found"))
-            .find(|d| d.as_str().eq("sma200"))
-            .expect("sma200 not found");
-        assert_eq!(nodes, "sma200")
     }
 }
