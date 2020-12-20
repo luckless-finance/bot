a82c23a9512faab1b7a4d047f51219104dbd4b43
grahamcrowell <graham.crowell@gmail.com>
2020-12-19 21:16:26 -0500

    move handler methods

--- src/bot.rs
+++ src/bot.rs
@@ -41,147 +41,67 @@ impl ExecutableBot {
     fn execute(&mut self) -> Result<(), String> {
         let calc_order = self.dag.execution_order().clone();
         for calc_name in calc_order {
-            // println!("\nexecuting {}", calc_name);
+            println!("\nexecuting {}", calc_name);
             self.set_status(&calc_name, CalculationStatus::InProgress);
-            let calc = self.calc_lkup.get(&calc_name).unwrap();
-            // println!("{:?}", calc?.operation());
+            let calc = self.calc_lkup.get(&calc_name).expect("calc not found");
 
             let calc_time_series = match calc.operation() {
-                Operation::DIV => foo_handle_div(
-                    calc,
-                    &self.calc_data_lkup,
-                    &self.data_client,
-                    &self.asset,
-                    &self.timestamp,
-                ),
-                Operation::SMA => foo_handle_sma(
-                    calc,
-                    &self.calc_data_lkup,
-                    &self.data_client,
-                    &self.asset,
-                    &self.timestamp,
-                ),
-                Operation::SUB => foo_handle_sub(
-                    calc,
-                    &self.calc_data_lkup,
-                    &self.data_client,
-                    &self.asset,
-                    &self.timestamp,
-                ),
-                Operation::QUERY => foo_handle_query(
-                    calc,
-                    &self.calc_data_lkup,
-                    &self.data_client,
-                    &self.asset,
-                    &self.timestamp,
-                ),
+                Operation::DIV => self.handle_div(calc),
+                Operation::SMA => self.handle_sma(calc),
+                Operation::SUB => self.handle_sub(calc),
+                Operation::QUERY => self.handle_query(calc),
             };
-
-            if calc_time_series.is_err() {
-                self.set_status(&calc_name, CalculationStatus::Error);
-            };
+            self.set_status(
+                &calc_name,
+                match calc_time_series.is_ok() {
+                    true => CalculationStatus::Complete,
+                    false => CalculationStatus::Error,
+                },
+            );
 
             self.calc_data_lkup
                 .insert(calc_name.clone(), calc_time_series?);
         }
         Ok(())
     }
-    // fn handle_div(&mut self, calc: &CalculationDTO) -> Result<TimeSeries1D, &str> {
-    //     assert_eq!(*calc.operation(), Operation::DIV);
-    //     println!("TODO execute {}", calc.name());
-    //     let values = vec![5., 10., 15.];
-    //     let index = vec![1, 3, 4];
-    //     Ok(TimeSeries1D::new(index, values))
-    // }
-    // fn handle_sma(&mut self, calc: &CalculationDTO) -> Result<TimeSeries1D, &str> {
-    //     assert_eq!(*calc.operation(), Operation::SMA);
-    //     println!("TODO execute {}", calc.name());
-    //     let values = vec![5., 10., 15.];
-    //     let index = vec![1, 3, 4];
-    //     Ok(TimeSeries1D::new(index, values))
-    // }
-    // fn handle_sub(&mut self, calc: &CalculationDTO) -> Result<TimeSeries1D, &str> {
-    //     assert_eq!(*calc.operation(), Operation::SUB);
-    //     println!("TODO execute {}", calc.name());
-    //     let values = vec![5., 10., 15.];
-    //     let index = vec![1, 3, 4];
-    //     Ok(TimeSeries1D::new(index, values))
-    // }
-    // fn handle_query(&mut self, calc: &CalculationDTO) -> Result<TimeSeries1D, &str> {
-    //     assert_eq!(*calc.operation(), Operation::QUERY);
-    //     println!("TODO execute {}", calc.name());
-    //     let name = "field";
-    //     // TODO parameterized query
-    //     let _field: &str = calc
-    //         .operands()
-    //         .iter()
-    //         .find(|o| o.name() == name)
-    //         .expect("symbol operand not found")
-    //         .value();
-    //     self.data_client.query(&self.asset, &TODAY)
-    // }
-}
 
-fn foo_handle_div(
-    calc: &CalculationDTO,
-    calc_data_lookup: &HashMap<String, TimeSeries1D>,
-    data_client: &MockDataClient,
-    asset: &Asset,
-    timestamp: &TimeStamp,
-) -> Result<TimeSeries1D, String> {
-    assert_eq!(*calc.operation(), Operation::DIV);
-    println!("TODO execute {}", calc.name());
-    let values = vec![5., 10., 15.];
-    let index = vec![1, 3, 4];
-    Ok(TimeSeries1D::new(index, values))
-}
+    fn handle_div(&self, calc: &CalculationDTO) -> Result<TimeSeries1D, String> {
+        assert_eq!(*calc.operation(), Operation::DIV);
+        println!("TODO execute {}", calc.name());
+        let values = vec![5., 10., 15.];
+        let index = vec![1, 3, 4];
+        Ok(TimeSeries1D::new(index, values))
+    }
 
-fn foo_handle_sma(
-    calc: &CalculationDTO,
-    calc_data_lookup: &HashMap<String, TimeSeries1D>,
-    data_client: &MockDataClient,
-    asset: &Asset,
-    timestamp: &TimeStamp,
-) -> Result<TimeSeries1D, String> {
-    assert_eq!(*calc.operation(), Operation::SMA);
-    println!("TODO execute {}", calc.name());
-    let values = vec![5., 10., 15.];
-    let index = vec![1, 3, 4];
-    Ok(TimeSeries1D::new(index, values))
-}
+    fn handle_sma(&self, calc: &CalculationDTO) -> Result<TimeSeries1D, String> {
+        assert_eq!(*calc.operation(), Operation::SMA);
+        println!("TODO execute {}", calc.name());
+        let values = vec![5., 10., 15.];
+        let index = vec![1, 3, 4];
+        Ok(TimeSeries1D::new(index, values))
+    }
 
-fn foo_handle_sub(
-    calc: &CalculationDTO,
-    calc_data_lookup: &HashMap<String, TimeSeries1D>,
-    data_client: &MockDataClient,
-    asset: &Asset,
-    timestamp: &TimeStamp,
-) -> Result<TimeSeries1D, String> {
-    assert_eq!(*calc.operation(), Operation::SUB);
-    println!("TODO execute {}", calc.name());
-    let values = vec![5., 10., 15.];
-    let index = vec![1, 3, 4];
-    Ok(TimeSeries1D::new(index, values))
-}
+    fn handle_sub(&self, calc: &CalculationDTO) -> Result<TimeSeries1D, String> {
+        assert_eq!(*calc.operation(), Operation::SUB);
+        println!("TODO execute {}", calc.name());
+        let values = vec![5., 10., 15.];
+        let index = vec![1, 3, 4];
+        Ok(TimeSeries1D::new(index, values))
+    }
 
-fn foo_handle_query(
-    calc: &CalculationDTO,
-    calc_data_lookup: &HashMap<String, TimeSeries1D>,
-    data_client: &MockDataClient,
-    asset: &Asset,
-    timestamp: &TimeStamp,
-) -> Result<TimeSeries1D, String> {
-    assert_eq!(*calc.operation(), Operation::QUERY);
-    println!("TODO execute {}", calc.name());
-    let name = "field";
-    // TODO parameterized query
-    let _field: &str = calc
-        .operands()
-        .iter()
-        .find(|o| o.name() == name)
-        .expect("symbol operand not found")
-        .value();
-    data_client.query(&asset, &timestamp)
+    fn handle_query(&self, calc: &CalculationDTO) -> Result<TimeSeries1D, String> {
+        assert_eq!(*calc.operation(), Operation::QUERY);
+        println!("TODO execute {}", calc.name());
+        let name = "field";
+        // TODO parameterized query
+        let _field: &str = calc
+            .operands()
+            .iter()
+            .find(|o| o.name() == name)
+            .expect("symbol operand not found")
+            .value();
+        self.data_client.query(&self.asset, &self.timestamp)
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -244,23 +164,6 @@ impl Bot {
                 .collect(),
             calc_data_lkup: HashMap::new(),
         }
-
-        // let calc_order = execution_order(self.dag());
-
-        //
-        // let _dag_node_output_lookup: HashMap<String, TimeSeries1D> = HashMap::new();
-        //
-        // let _dag_node_output_lookup: HashMap<String, TimeSeries1D> = self
-        //     .queries()
-        //     .iter()
-        //     .map(|c| {
-        //         (
-        //             c.name().to_string(),
-        //             self.data_client.query(asset, timestamp),
-        //         )
-        //     })
-        //     .collect();
-        // let _score_calc = self.strategy.score().calc();
     }
 }
 
