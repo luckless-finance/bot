9fb30bd3e7f3aa9165582ea100a580837665028a
grahamcrowell <graham.crowell@gmail.com>
2021-01-04 01:18:15 -0500

    wip - merge time series with itertools

--- src/time_series/generic_time_series.rs
+++ src/time_series/generic_time_series.rs
@@ -3,16 +3,18 @@
 use std::cmp::Ordering;
 use std::collections::{BTreeMap, HashMap};
 use std::fmt::Debug;
-use std::ops::Add;
+use std::iter::{FromIterator, FusedIterator};
+use std::ops::{Add, Range};
 
 use chrono::{DateTime, Utc};
+use itertools::Itertools;
 
 type TimeSeriesMap = TimeSeries<DateTime<Utc>, HashMap<String, f64>>;
 type TimeSeries1D = TimeSeries<DateTime<Utc>, f64>;
 
 #[derive(Debug, Clone, PartialEq)]
 struct TimeSeries<K, V>
-where
+    where
     K: Ord + Sized + Debug + Clone + PartialEq,
     V: Sized + Debug + Clone + PartialEq,
 {
@@ -29,34 +31,13 @@ where
             data: BTreeMap::new(),
         }
     }
-
-    pub fn align(&self) -> TimeSeries<K, V> {
-        TimeSeries::new()
+    pub fn from_vec(k: Vec<K>, v: Vec<V>) -> TimeSeries<K, V> {
+        let x: Range<i32> = 0..0;
+        TimeSeries {
+            data: BTreeMap::from_iter(k.into_iter().zip(v.into_iter())),
+        }
     }
 }
-
-// https://doc.rust-lang.org/std/ops/trait.Add.html
-// impl<K, V> Add for TimeSeries<K, V>
-//     where
-//         K: Ord + Sized + Debug + Clone + PartialEq,
-//         V: Sized + Debug + Clone + PartialEq + Add<Output=V>
-// {
-//     type Output = Self;
-//     fn add(self, rhs: Self) -> Self::Output {
-//         // self.data.keys()
-//         // self.data.append()
-//         // let left_index = self.data.first_key_value()?.0;
-//         // let right_index = rhs.data.first_key_value()?.0;
-//         // let i = match left_index.cmp(&right_index) {
-//         //     Ordering::Equal => left_index,
-//         //     _ => left_index
-//         // };
-//         Self::new();
-//         Self {
-//             data: self.data
-//         }
-//     }
-// }
 
 impl<K> TimeSeries<K, f64> where K: Ord + Sized + Debug + Clone + PartialEq {}
 
@@ -78,6 +59,8 @@ mod test {
     use std::ops::Add;
 
     use chrono::prelude::*;
+    use itertools::EitherOrBoth::{Both, Left, Right};
+    use itertools::Itertools;
 
     use crate::time_series::generic_time_series::*;
 
@@ -91,5 +74,85 @@ mod test {
             .insert(time_series_1d.data.keys().next().unwrap().clone(), 1.0);
         assert_eq!(time_series, time_series_1d);
         assert_eq!(time_series.clone() + time_series_1d.clone(), time_series_1d);
+    }
+
+    #[test]
+    fn merge_by() {
+        let bcf = TimeSeries::from_vec(vec!['b', 'c', 'f'], vec![1, 4, 2]).data;
+        println!("bcf={:?}", bcf);
+        let abc = TimeSeries::from_vec(vec!['a', 'b', 'c'], vec![1, 4, 2]).data;
+        println!("abc={:?}", abc);
+
+        let merge_by: Vec<(char, i32)> = abc
+            .into_iter()
+            .merge_by(bcf.into_iter(), |a, b| a.0 <= b.0)
+            .collect();
+        println!("merge_by={:?}", merge_by);
+        let expected_merge_by = vec![('a', 1), ('b', 4), ('b', 1), ('c', 2), ('c', 4), ('f', 2)];
+        assert_eq!(merge_by, expected_merge_by)
+    }
+
+    #[test]
+    fn merge_by_coalesce() {
+        let bcf = TimeSeries::from_vec(vec!['b', 'c', 'f'], vec![1, 4, 2]).data;
+        println!("bcf={:?}", bcf);
+        let abc = TimeSeries::from_vec(vec!['a', 'b', 'c'], vec![1, 4, 2]).data;
+        println!("abc={:?}", abc);
+
+        let merge_by: Vec<(char, i32)> = abc
+            .into_iter()
+            .merge_by(bcf.into_iter(), |a, b| a.0 <= b.0)
+            .coalesce(|a, b| {
+                if a.0 == b.0 {
+                    Ok((a.0, a.1 + b.1))
+                } else {
+                    Err((a, b))
+                }
+            })
+            .collect();
+        println!("merge_by={:?}", merge_by);
+        let expected_merge_by = vec![('a', 1), ('b', 5), ('c', 6), ('f', 2)];
+        assert_eq!(merge_by, expected_merge_by)
+    }
+
+    #[test]
+    fn merge_by_coalesce_to_map() {
+        let bcf = TimeSeries::from_vec(vec!['b', 'c', 'f'], vec![1, 4, 2]).data;
+        println!("bcf={:?}", bcf);
+        let abc = TimeSeries::from_vec(vec!['a', 'b', 'c'], vec![1, 4, 2]).data;
+        println!("abc={:?}", abc);
+
+        let merge_by: BTreeMap<char, i32> = abc
+            .into_iter()
+            .merge_by(bcf.into_iter(), |a, b| a.0 <= b.0)
+            .coalesce(|a, b| {
+                if a.0 == b.0 {
+                    Ok((a.0, a.1 + b.1))
+                } else {
+                    Err((a, b))
+                }
+            })
+            .collect();
+        println!("merge_by={:?}", merge_by);
+        assert_eq!(merge_by.first_key_value().unwrap(), (&'a', &1));
+        assert_eq!(merge_by.get(&'b').unwrap(), &5);
+        assert_eq!(merge_by.last_key_value().unwrap(), (&'f', &2));
+    }
+
+
+    #[test]
+    fn merge_by_group_by() {
+        let bcf = TimeSeries::from_vec(vec!['b', 'c', 'f'], vec![1, 4, 2]).data;
+        println!("bcf={:?}", bcf);
+        let abc = TimeSeries::from_vec(vec!['a', 'b', 'c'], vec![1, 4, 2]).data;
+        println!("abc={:?}", abc);
+
+        let merge_by: HashMap<char, Vec<(char, i32)>> = abc
+            .into_iter()
+            .merge_by(bcf.into_iter(), |a, b| a.0 <= b.0)
+            .into_group_map_by(|a| a.0);
+        println!("merge_by={:?}", merge_by);
+        assert_eq!(merge_by[&'a'], vec![('a', 1)]);
+        assert_eq!(merge_by[&'b'], vec![('b', 4), ('b', 1)]);
     }
 }
